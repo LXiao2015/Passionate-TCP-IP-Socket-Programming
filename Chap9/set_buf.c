9b463dfa1492ec13488b079c32700fe7de2b5499
Xiao Liang <m15801021028@163.com>
2020-09-02 03:15:36 -0700

    Chap9 example

---
+++ Chap9/set_buf.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    socklen_t len;
+    int state, snd_buf=1024*32, rcv_buf=1024*32;
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    len = sizeof(snd_buf);
+    state = setsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, len);
+    if (state)
+        error_handling("getsockopt() error!");
+
+    len = sizeof(rcv_buf);
+    state = setsockopt(sock, SOL_SOCKET, SO_RCVBUF, (void*)&rcv_buf, len);
+    if (state)
+        error_handling("getsockopt() error!");
+
+    len = sizeof(snd_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_SNDBUF, (void*)&snd_buf, &len);
+    if (state)
+        error_handling("getsockopt() error!");
+
+    len = sizeof(rcv_buf);
+    state = getsockopt(sock, SOL_SOCKET, SO_RCVBUF, (void*)&rcv_buf, &len);
+    if (state)
+        error_handling("getsockopt() error!");
+
+    printf("Input buffer size: %d \n", rcv_buf);
+    printf("Output buffer size: %d \n", snd_buf);
+
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
